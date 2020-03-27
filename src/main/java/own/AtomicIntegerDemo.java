d9f1c309bdb8f597ed5d4ccc88d5ca1de6456b80
zifeng <zifeng.1024@gmail.com>
2020-03-27 21:47:07 +0800

    just commit.

---
+++ src/main/java/own/AtomicIntegerDemo.java
@@ -0,0 +1,28 @@
+package own;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author yangzifeng
+ */
+public class AtomicIntegerDemo {
+    static volatile AtomicInteger atomicInteger = new AtomicInteger();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(AtomicIntegerDemo::add);
+        Thread thread2 = new Thread(AtomicIntegerDemo::add);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(atomicInteger.get());
+    }
+
+    static void add() {
+        int idx = 0;
+        while(idx++ < 10000) {
+            atomicInteger.getAndIncrement();
+        }
+    }
+
+}
---
+++ src/main/java/own/atomic/AtomicUser.java
@@ -0,0 +1,54 @@
+package own.atomic;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+class User {
+    String username;
+    int age;
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+public class AtomicUser {
+
+    public static void main(String[] args) {
+        User marry = new User("marry", 25);
+        User bob = new User("bob", 27);
+
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+        atomicReference.set(marry);
+
+        System.out.println(atomicReference.compareAndSet(marry, bob) + "\t" + atomicReference.get().toString());
+        System.out.println(atomicReference.compareAndSet(marry, bob) + "\t" + atomicReference.get().toString());
+    }
+}
